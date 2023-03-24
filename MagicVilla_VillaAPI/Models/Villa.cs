9455ac4cac57386471708f900e72085a8d5e68fe
Chris Keppler <ckkeppler@gmail.com>
2023-03-24 10:21:58 -0400

    add routes

---
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MagicVilla_VillaAPI.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int Occupancy { get; set; }
+        public int Sqft { get; set; }
+    }
+}
+
